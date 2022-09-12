817f647f02e50335f084059162cbe0807d815e1a
Ntaoleng <n_moloi@outlook.com>
2022-09-12 16:55:46 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include  <stdio.h>
+
+/**
+* main - Prints "with proper grammar, 
+* but the outcome is a piece of art,"
+* followed by a new line
+* Return: 0
+*/
+
+int main(void)
+{
+        char str[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+        printf(str);
+        return (0);
+}
