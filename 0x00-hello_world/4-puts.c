64949cc3a44bb093e7f786b05ce49561d9badb8e
Ntaoleng <n_moloi@outlook.com>
2022-09-12 16:28:45 -0700

    code fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include  <stdio.h>
 
-int main()
+/* main function prints "Programming is like building a multilingual puzzle, followed by a new line and returns 0 */
+
+int main(void)
 {
-        char str[] = "Programming is like building a multilingual puzzle";
-
+	char str[] = "\"Programming is like building a multilingual puzzle";
         puts(str);
-
-        return 0;
+	return (0);
 }
