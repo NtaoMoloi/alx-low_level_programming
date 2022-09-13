718a0da181d3d8b9b3eb2c782e5debb1953fa123
Ntaoleng <n_moloi@outlook.com>
2022-09-12 17:02:29 -0700

    fix error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,8 +9,8 @@
 
 int main(void)
 {
-        char str[] = "with proper grammar, but the outcome is a piece of art,\n";
+        char str[] = "with proper grammar, but the outcome is a piece of art,";
 
-        printf(str);
+        printf("%s\n",str);
         return (0);
 }
